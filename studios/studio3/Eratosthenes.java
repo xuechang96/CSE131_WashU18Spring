01f1d4dd3187d7d09415bb3cde18cb406db8a29c
xuec1 <xuec1@DESKTOP-94QP2HV.dhcp.wustl.edu>
2018-01-30 15:42:25 -0600

    Studio 3 finished

---
+++ studios/studio3/Eratosthenes.java
@@ -0,0 +1,48 @@
+package studio3;
+
+import cse131.ArgsProcessor;
+
+public class Eratosthenes {
+
+	public static void main(String[] args) {
+		ArgsProcessor ap=new ArgsProcessor(args);
+		int N=ap.nextInt("The size n");
+		int []contents=new int[N];
+		for (int i=0;i<N;++i) {
+			contents[i]=i+2;
+		}
+		for (int n=0;n<N;++n) {
+			if(n>2&&n%2==0) {
+				contents[n-2]=0;
+			}
+			if(n>3&&n%3==0) {
+				contents[n-2]=0;
+			}
+			if(n>5&&n%5==0) {
+				contents[n-2]=0;
+			}
+			if(n>7&&n%7==0) {
+				contents[n-2]=0;
+			}
+		}
+		System.out.print("Primes under "+(N+2)+" is: ");
+		for (int n=0;n<N;++n) {
+		if (contents[n]!=0) {
+			System.out.print(contents[n]+" ");
+		}	
+	}
+		System.out.println("");
+		int A=ap.nextInt("Decide whether the number is a prime number");
+		if(A>(N+1)||A<0) {
+			System.out.println("Error");
+		}
+		else {
+		if(contents[A-2]==0) {
+			System.out.println(A+": this is not a prime number");
+		}
+		else {
+			System.out.println(A+": this is a prime number");
+		}
+	}
+}
+}
