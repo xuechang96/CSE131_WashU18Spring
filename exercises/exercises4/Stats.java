e7a7c842a79f2e459d5b343a721c88e416c45da6
xuec1 <xuec1@DESKTOP-94QP2HV.dhcp.wustl.edu>
2018-01-30 18:08:42 -0600

    exercise4

--- exercises/exercises4/Stats.java
+++ exercises/exercises4/Stats.java
@@ -8,7 +8,26 @@ public class Stats {
 	public static void main(String[] args) {
 		// prompt the user for the file to be used for this run
 		ArgsProcessor.useStdInput("datafiles/average");
-
+		double sum=0;
+		int count=0;
+		double max=Double.MIN_VALUE;
+		double min=Double.MAX_VALUE;
+		while(!StdIn.isEmpty()) {
+			double d=StdIn.readDouble();
+			if(d>max) {
+				max=d;
+			}
+			if(d<min) {
+				min=d;
+			}
+			sum=sum+d;
+			count=count+1;
+		}
+		System.out.println("Sum is "+sum);
+		System.out.println("Count is "+count);
+		System.out.println("Average is "+sum/count);
+		System.out.println("Max value is "+max);
+		System.out.println("Min value is "+min);
 		//
 		//  Read in the data from the opened file, computing the
 		//     sum, average, count, max, and min
