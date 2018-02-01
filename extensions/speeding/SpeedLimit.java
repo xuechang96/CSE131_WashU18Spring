66a5cdff310d7d6141aa8d3867342f4a8c6c9a0e
xuec1 <xuec1@DESKTOP-94QP2HV.dhcp.wustl.edu>
2018-01-31 20:36:30 -0600

    extension 1 finished

--- extensions/speeding/SpeedLimit.java
+++ extensions/speeding/SpeedLimit.java
@@ -6,19 +6,23 @@ public class SpeedLimit {
 
 	public static void main(String[] args) {
 		ArgsProcessor ap=new ArgsProcessor(args);
-		double v=ap.nextDouble("The speed of you");
-		int l=ap.nextInt("The limit of the road");
+		int v=ap.nextInt("The speed of you (MPH)");
+		int l=ap.nextInt("The limit of the road (MPH)");
 		if (v<l) {
-			System.out.println("No fine!");
+			System.out.println("You reported a speed of "+v+" MPH for a speed limit of "+l+" MPH, no fine!");
 		}
 		else {
 			if (v-l<10){
-				System.out.println("The fine is $50");	
+				System.out.println("You reported a speed of "+v+" MPH for a speed limit of "+l+" MPH. ");	
+				System.out.println("You went "+(v-l)+" MPH over the speed limit.");
+				System.out.println("Your fine is $50.");
 			}
 			else {
-				double a=v-l-10;
-				double f=10*a+50;
-				System.out.println("The fine is $"+f);
+				int a=v-l-10;
+				int f=10*a+50;
+				System.out.println("You reported a speed of "+v+" MPH for a speed limit of "+l+" MPH. ");	
+				System.out.println("You went "+(v-l)+" MPH over the speed limit.");
+				System.out.println("Your fine is $"+f+".");
 			}
 		}
 		
