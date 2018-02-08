f98aada20ebb194822aff71c6385a9180cc047bd
xuec1 <xuec1@192.168.1.36>
2018-02-08 11:33:26 -0600

    .

--- exercises/exercises4/MouseFollower.java
+++ exercises/exercises4/MouseFollower.java
@@ -9,20 +9,35 @@ public class MouseFollower {
 
 	public static void main(String[] args) {
 		
+		ArgsProcessor ap=new ArgsProcessor(args);
+		int n=ap.nextInt("How much history?");
 		
+		double [] histX=new double[n];
+		double [] histY=new double [n];
+		int cur=0;
 		while (true) {
 			StdDraw.clear();
 			//
 			// Render one frame of your animation below here
 			//
-
-			
+			double x=StdDraw.mouseX();
+			double y=StdDraw.mouseY();
+			histX[cur]=x;
+			histY[cur]=y;
+			cur++;
+			if (cur>=n) {
+				cur=0;
+			}
+			int older=cur;
+			StdDraw.setPenColor(Color.blue);
+			StdDraw.filledCircle(x=histX[older], histY[older], .05);
+			StdDraw.show(10);
 			//  
 			// End of your frame
 			//
 			// Stdraw.show(..) achieves double buffering and
 			//   avoids the tight spinning loop
-			StdDraw.show(10);
+	
 		}
 
 	}
--- exercises/exercises4/Pitches.java
+++ exercises/exercises4/Pitches.java
@@ -8,7 +8,16 @@ public class Pitches {
 		// encoding has 0 as concert A
 		//    1 would be Bb, just above A
 		//   -1 would be Ab, just below A
-
+		ArgsProcessor ap=new ArgsProcessor(args);
+		int p=ap.nextInt("Enter an integer representing a pitch.");
+		double p7=p+7;
+		double f=440.0*Math.pow(2, p/12.0);
+		double f7=440.0*Math.pow(2, p7/12.0);
+		System.out.println("The pitch is "+p);
+		System.out.println("The frequecy is "+f);
+		System.out.println("The diatonic fifth "+f7);
+		double r=f7/f;
+		System.out.println("the ration is "+r);
 
 	}
 
