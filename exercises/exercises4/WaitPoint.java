111016e5963c28fbefeb3204df83ae9e833ec651
xuec1 <xuec1@192.168.1.36>
2018-02-07 22:27:15 -0600

    ,,

--- exercises/exercises4/WaitPoint.java
+++ exercises/exercises4/WaitPoint.java
@@ -1,20 +1,36 @@
 package exercises4;
 
+import java.awt.Color;
+
 import sedgewick.StdDraw;
 
 public class WaitPoint {
 
 	public static void main(String[] args) {
+		
 		// wait for the mouse to be pressed and released
-
+		while (!StdDraw.mousePressed()) {	
+		}
+		System.out.println("is Pressed");
 		// here, the mouse has been pressed
-
+		while(StdDraw.mousePressed()) {
+			//do nothing
+		}
 		// here the mouse is released
-		
-		
+		System.out.println("is Released");
+
+		double x=StdDraw.mouseX();
+		double y=StdDraw.mouseY();
 		// draw a point at the location of the mouse
-		
-		
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.point(x,y);
+		while(!StdDraw.hasNextKeyTyped()) {
+			//do nothing
+		}
+		if (StdDraw.nextKeyTyped()=='q') {
+			StdDraw.text(x-0.5, y-0.5, "good bye!");
+		}
 		// here, a q has been typed
 		
 		
