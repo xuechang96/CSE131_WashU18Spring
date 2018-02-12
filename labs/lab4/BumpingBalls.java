c97d52d323e1d0b99e0ff5117eecff80d349847a
xuec1 <xuec1@192.168.1.36>
2018-02-11 22:46:28 -0600

    extension

--- labs/lab4/BumpingBalls.java
+++ labs/lab4/BumpingBalls.java
@@ -1,9 +1,22 @@
 package lab4;
 
+import java.awt.Color;
+
+import cse131.ArgsProcessor;
+import sedgewick.StdDraw;
+
 public class BumpingBalls {
 	
 	public static void main(String[] args) {
-		
+		ArgsProcessor ap=new ArgsProcessor(args);
+		int n=ap.nextInt("The number of balls");
+		int i=ap.nextInt("The number of iterations");
+		StdDraw.setPenColor(Color.orange);
+		for (int b=1;b<=n;b++) {
+		double x= Math.random();
+		double y=Math.random();
+		StdDraw.filledCircle(x, y, 0.04);
+		}
 	}
 
 }
