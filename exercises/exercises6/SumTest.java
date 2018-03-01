e87bf2804a564428b2cbfdae9d0aeb70ad78e424
xuec1 <xuec1@192.168.1.36>
2018-03-01 10:24:57 -0600

    ,

--- exercises/exercises6/SumTest.java
+++ exercises/exercises6/SumTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SumTest {
-
+	
+	//@Test directive below is necessary for the method below it to be run as a unit test
 	@Test
 	public void test() {
 		assertEquals(0,Sum.sum(0));
