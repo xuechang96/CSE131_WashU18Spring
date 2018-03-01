088950744ca8b7f2436f1d5e6858e6d21f71c559
xuec1 <xuec1@192.168.1.36>
2018-03-01 10:53:02 -0600

    ,

--- exercises/exercises6/Add.java
+++ exercises/exercises6/Add.java
@@ -6,28 +6,41 @@ public class Add {
 	// Complete using the formula:
 	//   add(x,y) =           x , if y == 0
 	//   add(x,y) = add(x+1,y-1), otherwise
-	//
-	public static int add(int x, int y) {
-		return 0;  // FIXME
+	// require y>=0
+	public static int addhelper(int x, int y) {
+		if(y==0) {
+			return x;
+		}
+		else {
+			return addhelper(x+1,y-1);
+		}
 	}
 	
 	//
 	// Answer in comments below:
 	//
 	//  For what values of x and y does your method operate correctly?
-	//
+	//	x=1, y=1
 	//  Your answer:
-	//
+	//  2
 	//  Can you complete the following method
 	//   so that it calls add but returns the correct answer
 	//   for any x and y?
 	//  As a hint, consider that  (x+y) = -(-x + -y)
 	//
-	public static int addAny(int x, int y) {
-		if (y >= 0)
-			return add(x,y);
-		else
-			return 0; // FIXME
+	public static int add(int x, int y) {
+		if (y >= 0) {
+			return addhelper(x,y);
+		}
+		else {
+			if(x<0) {
+				return -addhelper(-x,-y);
+			}
+			else {
+				return addhelper(y,x);
+			}
+			
+		}
 	}
 
 }
--- exercises/exercises6/AddTest.java
+++ exercises/exercises6/AddTest.java
@@ -5,13 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AddTest {
-
+	//@Test directive below is necessary for the method below it to be run as a unit test
 	@Test
 	public void test() {
 		assertEquals(0, Add.add(0, 0));
-		//
+		for(int i=0;i<10;i++) {
+			assertEquals(i,Add.add(0, i));
+			assertEquals(i,Add.add(i, 0));
+		}
+	}
+	@Test
+	public void test2()
+	{		//
 		// write other test cases here:
 		//
+		for (int i=-100;i<100;i++) {
+			for (int j=-100;j<200;j++) {
+				assertEquals(i+j,Add.add(i, j));
+			}
+		}
+	}
+	@Test
+	public void test3() {
+		assertEquals(-2,Add.add(-4, 2));
+		assertEquals(-2,Add.add(2, -4));
 	}
 
 }
