6a465b93787e6a633bdcee9b1bf3e71f6c1aeabf
xuec1 <xuec1@192.168.1.36>
2018-03-22 00:52:38 -0500

    .

--- exercises/exercises7/Person.java
+++ exercises/exercises7/Person.java
@@ -6,33 +6,50 @@ package exercises7;
  *      Constructor
  *      Instance variables
  *      Methods
- *         Accessors
- *         Mutators
+ *         Accessors: return value of instance variables
+ *         Mutators: change value
  *         Other
  *
  */
 public class Person {
-	
-	private int height, age;
-	
+	/**
+	 * instance variables
+	 */
+	private int height, age;//instance variables
+	/**
+	 * constructor
+	 * @param height
+	 */
 	public Person(int height) {
 		this.height = height;
 		this.age    = 0;
 	}
-	
+	/**
+	 * mutator
+	 * @param height
+	 */
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
+	/**
+	 * other--ages by one year
+	 */
 	public void happyBirthday() {
 		this.age = this.age + 1;
 	}
 	
-	public int getHeight() {
+	/**
+	 * Accessors
+	 * @return
+	 */
+	public int getHeight() {//method
 		return this.height;
 	}
-	
-	public int getAge() {
+	/**
+	 * Accessors
+	 * @return
+	 */
+	public int getAge() {//method
 		return this.age;
 	}
 	
