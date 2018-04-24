700ccd4ce3e09ba8ccee308d76aae2f9d7004b62
xuec1 <xuec1@DESKTOP-94QP2HV.dhcp.wustl.edu>
2018-04-24 10:42:08 -0500

    .

--- extensions/eyeballs/Eyeballs.java
+++ extensions/eyeballs/Eyeballs.java
@@ -1,13 +1,8 @@
 package eyeballs;
-
 import java.awt.Color;
-
 import sedgewick.StdDraw;
 import cse131.ArgsProcessor;
-
 public class Eyeballs {
-	
-
 	/**
 	 * @param args
 	 */
@@ -15,52 +10,52 @@ public class Eyeballs {
 		ArgsProcessor ap = new ArgsProcessor(args);
 		StdDraw.picture(0.5, 0.5, "images/ken.jpg");
 		int N = ap.nextInt("How many eyeballs?");
-		
-		double eyes[][]=new double [2][N];
-		double m[][]=new double [2][N];
-		double d[][]=new double [2][N];
-		double dis[]=new double [N];
-		double p[][]=new double [2][N];
-		
-		
-		for( int i=0;i<N;i++) {			
-			StdDraw.setPenColor(Color.BLACK);
-			
-		while (!StdDraw.mousePressed()) {
-		    // do nothing, 
-		    // we are waiting for the mouse button to go down  
-		    //
-		    // The line below is important, as described below
-		    //
-		    StdDraw.show(50);
-		}
-		eyes[0][i]=StdDraw.mouseX();
-		eyes[1][i]=StdDraw.mouseY();
-		StdDraw.circle(eyes[0][i], eyes[1][i], 0.04);
-		StdDraw.setPenColor(Color.red);
-		StdDraw.filledCircle(eyes[0][i],eyes[1][i],0.01);
-		}
-	while(true) {			
-			StdDraw.clear();
-			StdDraw.picture(0.5, 0.5, "images/ken.jpg");
-			for(int i=0;i<N;i++) {
-				StdDraw.setPenColor(Color.black);
-				StdDraw.circle(eyes[0][i], eyes[1][i], 0.04);
+		double [] xx = new double [N];
+		double [] yy = new double [N];
+		int n=0;
+		while (true) {
+			if (n<N) {
+				while (StdDraw.mousePressed()) {
+					StdDraw.pause(100);
+				}
+				while (!StdDraw.mousePressed()) {
+					StdDraw.pause(100);
+				}
+				double x=StdDraw.mouseX();
+				double y=StdDraw.mouseY();
+				//x,y=the location of mouse of x and y.
+				StdDraw.setPenColor(Color.BLACK);
+				StdDraw.circle(x, y, 0.03);
+				StdDraw.setPenColor(Color.WHITE);
+				StdDraw.filledCircle(x, y, 0.03);
+				StdDraw.setPenColor(Color.BLACK);
+				StdDraw.filledCircle(x, y, 0.007);
+				yy[n]=y;
+				xx[n]=x;
+				n++;
 			}
-			for( int i=0;i<N;i++) {					
-				m[0][i]=StdDraw.mouseX();
-				m[1][i]=StdDraw.mouseY();
-				d[0][i]=m[0][i]-eyes[0][i];
-				d[1][i]=m[1][i]-eyes[1][i];
-				dis[i]=Math.pow((Math.pow(d[0][i], 2)+Math.pow(d[1][i], 2)),.5);
-				double r=0.03/dis[i];
-				p[0][i]=eyes[0][i]+d[0][i]*r;
-				p[1][i]=eyes[1][i]+d[1][i]*r;				
-				StdDraw.setPenColor(Color.red);
-				StdDraw.filledCircle(p[0][i],p[1][i],0.01);
-				StdDraw.show(10);
-		}	
+			else if (n==N) {
+				StdDraw.picture(0.5, 0.5, "images/ken.jpg");
+				for (int i=0; i<N; i++) {
+					double x=StdDraw.mouseX();
+					double y=StdDraw.mouseY();
+					x-=xx[i];
+					y-=yy[i];
+					double dis=Math.sqrt((Math.pow(x, 2)+Math.pow(y, 2)));
+					double d =0.01*Math.random()+0.007;
+					double sx=x/dis*d;
+					double sy=y/dis*d;
+					double fx=xx[i]+sx;
+					double fy=yy[i]+sy;
+					StdDraw.setPenColor(Color.BLACK);
+					StdDraw.circle(xx[i], yy[i], 0.03);
+					StdDraw.setPenColor(Color.WHITE);
+					StdDraw.filledCircle(xx[i],yy[i], 0.03);
+					StdDraw.setPenColor(Color.RED);
+					StdDraw.filledCircle(fx, fy, 0.008);
+				}
+				StdDraw.show(150);
+			}
 		}
 	}
 }
-
